32659adc138a3620a99871f690e6ac6cdbaa6b6e
mrskme <59914093+mrskme@users.noreply.github.com>
2021-04-09 13:06:30 +0200

    asdaweqwert

--- Edabit/Expert/MultipleChoiceTest/MultipleChoiceTest.cs
+++ Edabit/Expert/MultipleChoiceTest/MultipleChoiceTest.cs
@@ -23,27 +23,28 @@ namespace Edabit.Expert.MultipleChoiceTest
     {
         //https://edabit.com/challenge/thXMEAWNWyk9cCZcM
         public static void RunTheCode()
-        { 
-            var paper1 = new Testpaper("Maths", new [] { "1A", "2C", "3D", "4A", "5A" }, "60%"); 
-            var paper2 = new Testpaper("Chemistry", new [] {"1C", "2C", "3D", "4A"}, "75%"); 
-            var paper3 = new Testpaper("Computing", new [] {"1D", "2C", "3C", "4B", "5D", "6C", "7A"}, "75%");
-            
+        {
+            var paper1 = new Testpaper("Maths", new string[] { "1A", "2C", "3D", "4A", "5A" }, "60%");
+            var paper2 = new Testpaper("Chemistry", new string[] { "1C", "2C", "3D", "4A" }, "75%");
+            var paper3 = new Testpaper("Computing", new string[] { "1D", "2C", "3C", "4B", "5D", "6C", "7A" }, "75%");
+            var paper4 = new Testpaper("Physics", new string[] { "1A", "2B", "3A", "4C", "5A", "6C", "7A", "8C", "9D", "10A", "11A" }, "90%");
+
             var student1 = new Student(); 
             var student2 = new Student();
+            var student3 = new Student();
 
-            student1.TakeTest(paper1, new [] {"1A", "2D", "3D", "4A", "5A"});
+            student1.TakeTest(paper1, new[] {"1A", "2D", "3D", "4A", "5A"});
 
-            student2.TakeTest(paper2, new [] {"1C", "2D", "3A", "4C" });
-            student2.TakeTest(paper3, new [] {"1A", "2C", "3A", "4C", "5D", "6C", "7B" });
+            student2.TakeTest(paper2, new[] { "1C", "2D", "3A", "4C" });
+            student2.TakeTest(paper3, new[] { "1A", "2C", "3A", "4C", "5D", "6C", "7B" });
 
-            foreach (var testResult in student1.TestsTaken)
-            {
-                Console.WriteLine(testResult);
-            }
-            foreach (var testResult in student2.TestsTaken)
-            { 
-                Console.WriteLine(testResult);
-            }
+            student3.TakeTest(paper1, new[] { "1C", "2D", "3A", "4C", "5A" });
+            student3.TakeTest(paper3, new[] { "1A", "2C", "3A", "4C", "5D", "6C", "7B" });
+            student3.TakeTest(paper4, new[] { "1A", "2C", "3A", "4C", "5D", "6C", "7B", "8C", "9D", "10A", "11A" });
+
+            student1.WriteTestsTaken();
+            student2.WriteTestsTaken();
+            student3.WriteTestsTaken();
         }
     }
     public class Testpaper : ITestpaper
@@ -70,7 +71,6 @@ namespace Edabit.Expert.MultipleChoiceTest
         }
         public void TakeTest(ITestpaper paper, string[] testAnswers)
         {
-            //VERSION 3
             double correctAnswersCount = testAnswers.Where((t, i) => t == paper.MarkScheme[i]).Count();
             var percentageCorrect = Math.Round(correctAnswersCount * 100 / testAnswers.Length);
             var percentageToPass = Convert.ToInt32(paper.PassMark.Remove(paper.PassMark.Length - 1));
@@ -82,33 +82,14 @@ namespace Edabit.Expert.MultipleChoiceTest
             if (TestsTaken[0] == "No tests taken") TestsTaken.Clear();
             TestsTaken.Add(resultString);
             TestsTaken.Sort();
+        }
 
-
-
-
-
-
-
-            // VERSION 1
-            //var correctAnswersCount = 0;
-            //bool[] isAnswersCorrect = new bool[testAnswers.Length];
-            //int correctAnswersCount = 0;
-            //for (int i = 0; i < testAnswers.Length; i++)
-            //{
-            //    if (testAnswers[i].Equals(paper.MarkScheme[i])) isAnswersCorrect[i] = true;
-            //    else isAnswersCorrect[i] = false;
-            //}
-            //            foreach (var answer in isAnswersCorrect)
-            //            {
-            //                if (answer) correctAnswersCount++;
-            //            }
-
-            // VERSION 2
-            //var correctAnswersCount = 0;
-            //for (var i = 0; i < testAnswers.Length; i++)
-            //{
-            //    if (testAnswers[i] == paper.MarkScheme[i]) correctAnswersCount++;
-            //}
+        public void WriteTestsTaken()
+        {
+            foreach (var test in TestsTaken)
+            {
+                Console.WriteLine(test);
+            }
         }
     }
 }
